d348235e56005848bfcb47f54b67b69ec36dc735
JustinChen1975 <xcchen@xmu.edu.my>
2021-02-27 00:28:03 +0800

    ads

--- beater/capture.go
+++ beater/capture.go
@@ -398,6 +398,7 @@ func decodeAndPublish(packetDataChannel chan []byte, client beat.Client) {
 		//tghEvent["hw_id"] = ((uint16(packetData[63]) | uint16(packetData[62])<<8)>>6 ) & 0x003F
 		tghEvent["hw_id"] = (binary.BigEndian.Uint16(packetData[62:])>>6 ) & 0x003F
 		tghEvent["sequenceNumber"] = (binary.BigEndian.Uint32(packetData[62:])) & 0x003FFFFF
+		tghEvent["nodeId"] = binary.BigEndian.Uint32(packetData[66:])
 
 		udpEvent := common.MapStr{}
 		fields["udpRelated"] = udpEvent
@@ -462,12 +463,12 @@ func (e *Error) Error() string {
 	return "dns: " + e.err
 }
 
-func unpackUint16(msg []byte, off int) (i uint16, off1 int, err error) {
-	if off+2 > len(msg) {
-		return 0, len(msg), &Error{err: "overflow unpacking uint16"}
-	}
-	return binary.BigEndian.Uint16(msg[off:]), off + 2, nil
-}
+//func unpackUint16(msg []byte, off int) (i uint16, off1 int, err error) {
+//	if off+2 > len(msg) {
+//		return 0, len(msg), &Error{err: "overflow unpacking uint16"}
+//	}
+//	return binary.BigEndian.Uint16(msg[off:]), off + 2, nil
+//}
 
 //func unpackUint32(msg []byte, off int) (i uint32, off1 int, err error) {
 //	if off+4 > len(msg) {
